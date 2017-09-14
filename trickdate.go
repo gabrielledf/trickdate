f122accb04fc747b447a2314b6e0e95389a1f4d8
gabrielledf <gabrielledf@mpf.mp.br>
2017-09-14 16:35:21 -0300

    Inclusion of string cleanup support and formatting the date string from ddmmaaaa format to aaaammdd

--- trickdate.go
+++ trickdate.go
@@ -1,24 +1,49 @@
 package trickdate
 
 import (
-	//"time"
 	"regexp"
 )
 
-var reDate *regexp.Regexp
+var recleanDate, reDate, reDateFull *regexp.Regexp
 
 /*Func to starts regexp
 Date format accepted: dd/mm/yyyy without requiring separator character
 * */
 func init() {
-	reDate = regexp.MustCompile(`^(0[1-9]|[12][0-9]|3[01])[-|\\|/|\s]*(0[1-9]|1[012])[-|\\|/|\s]*(?:19|20)\d{2}?$`)
+	//reDate = regexp.MustCompile(`^(0[1-9]|[12][0-9]|3[01])[-|\\|/|\s]*(0[1-9]|1[012])[-|\\|/|\s]*(?:19|20)\d{2}?$`)
+	reDate = regexp.MustCompile(`^(0[1-9]|[12][0-9]|3[01])[-|\\|/|\s]*(0[1-9]|1[012])[-|\\|/|\s]*(19|20)(\d{2})$`)
+	reDateFull = regexp.MustCompile(`^(0[1-9]|[12][0-9]|3[01])[-|\\|/|\s]*(0[1-9]|1[012])[-|\\|/|\s]*\d{4}?$`)
+	recleanDate = regexp.MustCompile(`\D`)
 }
 
-//Verifies if the string is a valid date
+//Verifies if the string is a valid date - year between 1900-2099
 func IsDate(date string) bool {
 	return reDate.MatchString(date)
 }
 
-func FormatDate(date string) (string, error) {
-	return date, nil
+//Verifies if the string is a valid date
+func IsDateFull(date string) bool {
+	return reDateFull.MatchString(date)
+}
+
+//Removes non-numeric characters
+func cleanDate(date string) string {
+	return recleanDate.ReplaceAllString(date, "")
+}
+
+//Reverse the order ddmmaaaa to aaaammdd
+func FormatDate(date string) string {
+	var day, month, year string
+	var datePieces []string
+
+	datePieces = reDate.FindStringSubmatch(cleanDate(date))
+	if datePieces != nil {
+		day = datePieces[1]
+		month = datePieces[2]
+		year = datePieces[3] + datePieces[4]
+	} else {
+		return ""
+	}
+
+	return year + month + day
 }
--- trickdate_test.go
+++ trickdate_test.go
@@ -57,3 +57,75 @@ func TestIsDate(t *testing.T) {
 		t.Log(checkMark)
 	}
 }
+
+func TestIsDateFull(t *testing.T) {
+	for i, v := range []struct {
+		valueTested string
+		expected    bool
+	}{
+		// Invalid format.
+		{"11111111111", false},
+		{"", false},
+		{"#$%¨&*(ABCDEF", false},
+		{"2017-12-31", false},
+
+		// Common invalid patterns.
+		{"00000000", false},
+
+		// Requires two-digit daytime representation
+		{"1/10/1900", false},
+
+		// Requires two-digit month representation
+		{"10/1/1900", false},
+
+		// Requires four-digit year representation
+		{"10/01/17", false},
+
+		// The day can not be greater than 31
+		{"32/12/1900", false},
+
+		// The month can not be greater than 12
+		{"31/13/1900", false},
+
+		// Valid.
+		{"31/12/2017", true},
+		{"31 12 2017", true},
+		{"31-12-2017", true},
+		{"31122017", true},
+		{"31      12     2017", true},
+		{"31/////12/////2017", true},
+
+		// Accepts years
+		{"31/12/1889", true},
+		{"31/12/3123", true},
+	} {
+		t.Logf("#%d Data validation of %s should return %v: ", i, v.valueTested, v.expected)
+		got := IsDateFull(v.valueTested)
+		if got != v.expected {
+			t.Fatal(ballotX)
+		}
+		t.Log(checkMark)
+	}
+}
+
+func TestFormatDate(t *testing.T) {
+	for i, v := range []struct {
+		valueTested string
+		expected    string
+	}{
+		{"31/12/2017", "20171231"},
+		{"31 12 2017", "20171231"},
+		{"31-12-2017", "20171231"},
+		{"31122017", "20171231"},
+		{"31      12     2017", "20171231"},
+		{"31/////12/////2017", "20171231"},
+		{"19-05-1981", "19810519"},
+	} {
+		t.Logf("#%d Conversion date of %s should return %v: ", i, v.valueTested, v.expected)
+		got := FormatDate(v.valueTested)
+		if got != v.expected {
+			t.Fatal(ballotX)
+		}
+		t.Log(checkMark)
+	}
+}
